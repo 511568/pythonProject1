ecf122cd6004d7f789996a4b63312b6c4c8ca4ea
511568 <511568@qq.com>
2022-06-11 01:53:50 +0800

    chapter_8_test 8-10

--- chapter 8.py
+++ chapter 8.py
@@ -60,10 +60,100 @@ print(city)
 #练习8-7
 def make_album(singer,albumname):
     """创建歌手名和专辑名的字典"""
-    album = {'singer':singer,
-                 'albumname':albumname,
-             }
+    album = {
+        'singer':singer,
+        'albumname':albumname,
+    }
     print(album)
 make_album('singer1','album1')
 make_album('singer2','album2')
 make_album('singer3','album3')
+
+
+#！！！8-7 样本答案
+def make_album(artist, title):
+    """创建一个包含专辑信息的字典。"""
+    album_dict = {
+       'artist': artist.title(),        #1.设定字典键，并设定值为函数的值
+       'title': title.title(),
+       }
+    return album_dict                   #3.通过返回字典键值，赋值给album，并打印album
+    #return在这里充当了返回值的作用，当给出了函数实参后，album_dict里的键值被填充了，进一步album =使得填充的键值被赋值给了album
+
+
+album = make_album('metallica', 'ride the lightning')       #2.函数实参填入后，实参被传递到字典内
+print(album)
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+#8-7高级版
+def make_album(artist, title):
+    """创建一个包含专辑信息的字典。"""
+    album_dict = {
+       'artist': artist.title(),        #1.设定字典键，并设定值为函数的值
+       'title': title.title(),
+       }
+    return album_dict                   #3.通过返回字典键值，赋值给album，并打印album
+    #return在这里充当了返回值的作用，当给出了函数实参后，album_dict里的键值被填充了，进一步album =使得填充的键值被赋值给了album
+
+
+album = make_album('metallica', 'ride the lightning')       #2.函数实参填入后，实参被传递到字典内
+print(album)
+album = make_album('beethoven', 'ninth symphony')
+print(album)
+album = make_album('willie nelson', 'red-headed stranger')
+print(album)
+
+#8-8
+def make_album(artist, title):
+    """创建一个包含专辑信息的字典。"""
+    album_dict = {
+       'artist': artist.title(),
+       'title': title.title(),
+       }
+    return album_dict
+active = True
+while active:
+    artist = input("\n请输入歌手名称(可随时输入q来退出)： ")
+    if artist == 'q':
+        active = False
+    title = input("\n请输入专辑名称： ")
+    if title == 'q':
+        active = False
+    else:
+        print(make_album(artist,title))
+
+#8-9
+def show_message(messages):
+    for message in messages:
+        print(f"{message.title()}")
+
+
+messages_list = ['message1','message2','message3']
+show_message(messages_list)
+
+#8-10
+def show_message(messages):
+    for message in messages:
+        print(f"{message.title()}")
+
+def send_messages(messages,sent_messages):
+    while messages:
+        current_message = messages.pop()
+        print(f"已打印{current_message}")
+        sent_messages.append(current_message)
+
+messages_list = ['message1','message2','message3']
+show_message(messages_list)
+
+sent_messages = []
+send_messages(messages,sent_messages)
+
+print("\nFinal lists: ")
+print(messages_list)
+print(sent_messages)
+
+
+
